d1e10645debecc05350604c330c8ef718270d26c
Josh Dibble <32303700+jmdibble@users.noreply.github.com>
2019-02-24 13:01:10 +0000

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,163 @@
+// // FUNCTION CONSTRUCTOR
+
+// var john = {
+//     name: "John",
+//     yearOfBirth: 1990,
+//     job: "teacher",
+// };
+
+// var Person = function(name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
+
+// Person.prototype.calculateAge = function() {
+//     console.log(2016 - this.yearOfBirth);
+// }
+
+// var john = new Person("John", 1990, "teacher");
+
+// var jane = new Person("Jane", 1969, "designer");
+
+// var mark = new Person("Mark", 1948, "retired");
+
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+
+
+// Object.create
+
+// var personProto = {
+//     calculateAge: function() {
+//         console.log(2016 - this.yearOfBirth);
+//     }
+// };
+
+// var john = Object.create(personProto);
+
+// john.name = "John";
+// john.yearOfBirth = 1990;
+// john.job = "teacher";
+
+// var jane = Object.create(personProto, 
+// {
+//     name: { value: "Jane" },
+//     yearOfBirth: { value: 1969 },
+//     job: { value: "designer" },
+// });
+
+
+// Primatives vs. Objects
+
+// var a = 23;
+// var b = a;
+// a = 46;
+// console.log(a, b);
+
+// var obj1 = {
+//     name: "John",
+//     age: 26
+// };
+
+// var obj2 = obj1;
+// obj1.age = 30;
+// console.log(obj1.age);
+// console.log(obj2.age);
+
+// // Functions
+
+// var age = 27;
+// var obj = {
+//     name: "Jonas",
+//     city: "Lisbon"
+// };
+
+// function change(a, b) {
+//     a = 30;
+//     b.city = "San Francisco";
+// };
+
+// change(age, obj);
+
+// console.log(age, obj.city);
+
+// PASSING FUNCTIONS AS ARGUMENTS
+
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr, fn) {
+//     var arrRes = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+
+// function calculateAge(el) {
+//     return 2016 - el;
+// }
+
+// function isFullAge(el) {
+//     return el >= 18;
+// }
+
+// function maxHeartRate(el) {
+
+//     if (el >= 18 && el <= 81) {
+//         return Math.round(206.9 - (0.67 * el));
+//     } else {
+//         return -1;
+//     }
+// }
+
+
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
+// var rates = arrayCalc(ages, maxHeartRate);
+
+// console.log(ages);
+// console.log(fullAges);
+// console.log(rates);
+
+
+// FUNCTIONS RETURNING FUNCTIONS
+
+// function interviewQuestion(job) {
+//     if (job === "designer") {
+//         return function(name) {
+//             console.log(name + ", can you explain what UX design is?");
+//         }
+//     } else if (job === "teacher") {
+//         return function(name) {
+//             console.log("What subject do you teach, " + name + "?");
+//         } 
+//     } else {
+//         return function(name) {
+//             console.log("Hello, " + name + ", what do you do?");
+//         }
+//     }
+// }
+
+// var teacherQuestion = interviewQuestion("teacher");
+// var designerQuestion = interviewQuestion("designer");
+
+
+// teacherQuestion("John");
+// designerQuestion("John");
+
+// interviewQuestion("teacher")("Mark");
+
+
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+function game() {
+    var score = Math.random() * 10;
+    console.log(score>= 5);
+}
+
+game();
+
+
+
